4140e812480b205e7101d76b0b5a778a83a62f99
Rafli Iskandar Kavarera <52194252+Kavarera@users.noreply.github.com>
2024-03-07 10:15:49 +0700

    Delete TugasPertemuan2 directory

--- TugasPertemuan2/src/tugas/pertemuan2/Main.java
+++
@@ -1,9 +0,0 @@
-package tugas.pertemuan2;
-
-import tugas.pertemuan2.builder.QuizBuilder;
-
-public class Main {
-    public static void main(String[] args) {
-        QuizBuilder qb = new QuizBuilder();
-    }
-}
